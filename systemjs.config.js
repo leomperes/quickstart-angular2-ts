0062244ae62ca0209c0205e7f96e41303a7d4756
Leo M <leomperes@belanton.com>
2016-09-29 11:27:51 -0400

    Add ESLint config file; create index page and style sheet; and finish QuickStart

--- systemjs.config.js
+++ systemjs.config.js
@@ -11,7 +11,7 @@
         // map tells the System loader where to look for things
         map:      {
             // our app is within the app folder
-            app:                                 'app',
+            app:                                 'dist', //'app',
             // angular bundles
             '@angular/core':                     'npm:@angular/core/bundles/core.umd.js',
             '@angular/common':                   'npm:@angular/common/bundles/common.umd.js',
@@ -23,7 +23,7 @@
             '@angular/forms':                    'npm:@angular/forms/bundles/forms.umd.js',
             // other libraries
             'rxjs':                              'npm:rxjs',
-            'angular-in-memory-web-api':         'npm:angular-in-memory-web-api',
+            'angular-in-memory-web-api':         'npm:angular-in-memory-web-api'
         },
         // packages tells the System loader how to load when no filename and/or no extension
         packages: {
